35dc9deeb5b4f42c0a92bcc924f4919335edeb9c
vedantbhawsar <vedantbhavsar.a10@gmail.com>
2023-08-20 23:16:48 +0530

    banana jindabad

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,142 @@
-import Footer from "./assets/Footer"
-import NavBar from "./assets/NavBar"
-import News from "./assets/News"
-import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useRouteLoaderData
-} from "react-router-dom";
-
+import Footer from "./assets/Footer";
+import NavBar from "./assets/NavBar";
+import News from "./assets/News";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes, useRouteLoaderData } from "react-router-dom";
 
 export default class App extends Component {
+	apikey = process.env.REACT_APP_API_KEY;
 
-  apikey = 'd32bdc6b771c45b48ff94ec377af6c43';
+	state = {
+		country: "us"
+	};
 
-  state = {
-    country: "us"
-  }
+	in_filter = () => {
+		this.setState({ country: "in" });
+	};
 
-  in_filter = () => {
-    this.setState({ country: 'in' });
-  }
+	us_filter = () => {
+		this.setState({ country: "us" });
+	};
 
-  us_filter = () => {
-    this.setState({ country: 'us' });
-  }
+	ch_filter = () => {
+		this.setState({ country: "ch" });
+	};
 
-  ch_filter = () => {
-    this.setState({ country: 'ch' });
-  }
-
-  jp_filter = () => {
-    this.setState({ country: 'jp' });
-  }
-  render() {
-    return (
-      <div>
-        <Router>
-          <NavBar InFilter={this.in_filter} UsFilter={this.us_filter} JpFilter={this.jp_filter} ChFilter={this.ch_filter} />
-          <Routes>
-            <Route path="/" exact element={<News key="/" category='general' apikey={this.apikey} country={this.state.country} />} />
-            <Route path="/home" exact element={<News key="home" category='general' apikey={this.apikey} country={this.state.country} />} />
-            <Route path="/business" exact element={<News key="business" category='business' apikey={this.apikey} country={this.state.country} />} />
-            <Route path="/entertainment" exact element={<News key="entertainment" category='entertainment' apikey={this.apikey} country={this.state.country} />} />
-            <Route path="/health" exact element={<News key="health" category='health' apikey={this.apikey} country={this.state.country} />} />
-            <Route path="/science" exact element={<News key="science" category='science' apikey={this.apikey} country={this.state.country} />} />
-            <Route path="/sports" exact element={<News key="sports" category='sports' apikey={this.apikey} country={this.state.country} />} />
-            <Route path="/technology" exact element={<News key="technology" category='technology' apikey={this.apikey} country={this.state.country} />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </div>
-    )
-  }
+	jp_filter = () => {
+		this.setState({ country: "jp" });
+	};
+	render() {
+		return (
+			<div>
+				<Router>
+					<NavBar
+						InFilter={this.in_filter}
+						UsFilter={this.us_filter}
+						JpFilter={this.jp_filter}
+						ChFilter={this.ch_filter}
+					/>
+					<Routes>
+						<Route
+							path="/"
+							exact
+							element={
+								<News
+									key="/"
+									category="general"
+									apikey={this.apikey}
+									country={this.state.country}
+								/>
+							}
+						/>
+						<Route
+							path="/home"
+							exact
+							element={
+								<News
+									key="home"
+									category="general"
+									apikey={this.apikey}
+									country={this.state.country}
+								/>
+							}
+						/>
+						<Route
+							path="/business"
+							exact
+							element={
+								<News
+									key="business"
+									category="business"
+									apikey={this.apikey}
+									country={this.state.country}
+								/>
+							}
+						/>
+						<Route
+							path="/entertainment"
+							exact
+							element={
+								<News
+									key="entertainment"
+									category="entertainment"
+									apikey={this.apikey}
+									country={this.state.country}
+								/>
+							}
+						/>
+						<Route
+							path="/health"
+							exact
+							element={
+								<News
+									key="health"
+									category="health"
+									apikey={this.apikey}
+									country={this.state.country}
+								/>
+							}
+						/>
+						<Route
+							path="/science"
+							exact
+							element={
+								<News
+									key="science"
+									category="science"
+									apikey={this.apikey}
+									country={this.state.country}
+								/>
+							}
+						/>
+						<Route
+							path="/sports"
+							exact
+							element={
+								<News
+									key="sports"
+									category="sports"
+									apikey={this.apikey}
+									country={this.state.country}
+								/>
+							}
+						/>
+						<Route
+							path="/technology"
+							exact
+							element={
+								<News
+									key="technology"
+									category="technology"
+									apikey={this.apikey}
+									country={this.state.country}
+								/>
+							}
+						/>
+					</Routes>
+					<Footer />
+				</Router>
+			</div>
+		);
+	}
 }
--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -1,48 +1,75 @@
-import React, { Component } from 'react'
-import { BsFacebook, BsTwitter, BsGoogle, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs'
+import React, { Component } from "react";
+import { BsFacebook, BsTwitter, BsGoogle, BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs";
 export default class Footer extends Component {
-    render() {
-        return (
-            <div>
-                <footer className="bg-dark text-center text-white" style={{ marginTop: "10px " }}>
-                    <div className="container p-4 pb-0">
-                        <section className="mb-4">
+	render() {
+		return (
+			<div>
+				<footer className="bg-dark text-center text-white" style={{ marginTop: "10px " }}>
+					<div className="container p-4 pb-0">
+						<section className="mb-4">
+							<div
+								className="btn btn-outline-light btn-floating m-1"
+								href="#!"
+								role="button"
+							>
+								<BsFacebook />
+							</div>
 
-                            <div className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                            ><BsFacebook />
-                            </div>
+							<div
+								className="btn btn-outline-light btn-floating m-1"
+								href="#!"
+								role="button"
+							>
+								<BsTwitter />
+							</div>
 
+							<div
+								className="btn btn-outline-light btn-floating m-1"
+								href="#!"
+								role="button"
+							>
+								<BsGoogle />
+							</div>
 
-                            <div className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-                                <BsTwitter />
-                            </div>
+							<div
+								className="btn btn-outline-light btn-floating m-1"
+								href="#!"
+								role="button"
+							>
+								<BsInstagram />
+							</div>
 
+							<div
+								className="btn btn-outline-light btn-floating m-1"
+								href="#!"
+								role="button"
+							>
+								<BsLinkedin />
+							</div>
 
-                            <div className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                            ><BsGoogle /></div>
+							<div
+								className="btn btn-outline-light btn-floating m-1"
+								href="#!"
+								role="button"
+							>
+								<BsGithub />
+							</div>
+						</section>
+					</div>
 
-
-                            <div className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                            ><BsInstagram /></div>
-
-                            <div className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                            ><BsLinkedin /></div>
-
-                            <div className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                            ><BsGithub />
-                            </div>
-                        </section>
-
-                    </div>
-
-
-
-                    <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-                        © 2020 Copyright :
-                        <a className="text-white" href="banananewz.com" style={{ textDecoration: "none" }}>  BananaNewz.com</a>
-                    </div>
-                </footer>
-            </div>
-        )
-    }
+					<div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+						© 2020 Copyright :
+						<a
+							className="text-white"
+							href="banananewz.com"
+							style={{ textDecoration: "none" }}
+						>
+							{" "}
+							BananaNewz.com
+						</a>
+					</div>
+				</footer>
+			</div>
+		);
+	}
 }
